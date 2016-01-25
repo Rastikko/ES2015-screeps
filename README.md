e5f7b2ab543d6b4b8813d29392ca38f2a9762bfa
Johan <rastikkko@gmail.com>
2016-01-24 19:05:28 -0800

    Created a proof of concept with harvester for ES2015

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ES2015-screeps
 A simple screeps framework using ES2015
+
+```shell
+npm run build
+```
---
+++ src/harvester.js
@@ -0,0 +1,14 @@
+function harverster(creep) {
+	if (creep.carry.energy < creep.carryCapacity) {
+		let sources = creep.room.find(FIND_SOURCES);
+		if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+			creep.moveTo(sources[0]);
+		}
+	} else {
+		if (creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+			creep.moveTo(Game.spawns.Spawn1);
+		}
+	}
+}
+
+export default harvester;
