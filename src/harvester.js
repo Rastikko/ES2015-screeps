d80a78e0dc060ffc99b5d3698f96a8b27c9cfda1
Johan <rastikkko@gmail.com>
2016-01-24 19:10:16 -0800

    Remove unused option babelrc and fix typo harvester

--- src/harvester.js
+++ src/harvester.js
@@ -1,4 +1,4 @@
-function harverster(creep) {
+function harvester(creep) {
 	if (creep.carry.energy < creep.carryCapacity) {
 		let sources = creep.room.find(FIND_SOURCES);
 		if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
