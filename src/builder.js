4579c83a77a513dc1fc77341fdec34b217ac6a2f
Johan <rastikkko@gmail.com>
2016-01-27 01:17:02 -0800

    Adding transfer energy snippet to builder

--- src/builder.js
+++ src/builder.js
@@ -1,9 +1,18 @@
 function builder(roomGame, creep) {
-    if (creep.carry.energy === 0 && roomGame.getSpawnMemory('spammerFinished')) {
-    	if (Game.spawns.Spawn1.transferEnergy(creep) == ERR_NOT_IN_RANGE) {
-    		creep.moveTo(Game.spawns.Spawn1);
-    	}
-    } else {
+  let creepEmpty = creep.carry.energy === 0;
+	let spawnFinished = roomGame.getSpawnMemory('spammerFinished');
+	let transferEnergy = creep.memory.transferEnergy;
+
+	if ((creepEmpty || transferEnergy) && spawnFinished) {
+		if (Game.spawns.Spawn1.transferEnergy(creep) === ERR_NOT_IN_RANGE) {
+			creep.moveTo(Game.spawns.Spawn1);
+		}
+		if (creep.carry.energy !== creep.carryCapacity) {
+			creep.memory.transferEnergy = true;
+		} else {
+			creep.memory.transferEnergy = false;
+		}
+	} else {
     	let targets = creep.room.find(FIND_CONSTRUCTION_SITES);
     	if (targets.length) {
     		if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
