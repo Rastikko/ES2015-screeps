ff02eeda5f9b9ebed744a32a441606838e6278b2
johan <rastikko@gmail.com>
2016-01-27 20:23:00 -0800

    Rename lost var wall

--- src/builder.js
+++ src/builder.js
@@ -15,7 +15,7 @@ function builder(roomGame, creep) {
 	} else {
     	let targets = creep.room.find(FIND_CONSTRUCTION_SITES);
     	if (targets.length) {
-    		if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+    		if (creep.build(targets[0]) === ERR_NOT_IN_RANGE) {
     			creep.moveTo(targets[0]);
     		}
     	}
@@ -27,8 +27,8 @@ function builder(roomGame, creep) {
             }
         });
         if(halfBroken) {
-            if(creep.repair(wall) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(wall);
+            if(creep.repair(halfBroken) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(halfBroken);
             }
         }
       }
